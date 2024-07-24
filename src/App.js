bafb1a7f11d17b3721611854cbb6a231705f8345
Aayush-Koju <iush.cozu12@gmail.com>
2024-07-24 19:00:19 +0545

    modifying main content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        Welcome to Moviedux
       </header>
+      <footer className="footer">
+        <p className='footer'>Footer content goes here</p>
+      </footer>
     </div>
   );
 }
