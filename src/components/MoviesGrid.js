6c2d14887b28521d4dec6ec4314324c602ac7693
Aayush-Koju <iush.cozu12@gmail.com>
2024-07-25 21:59:29 +0545

    added js logic to render movie and display nicely on app

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -16,6 +16,21 @@ export default function MoviesGrid() {
 
 
     return(
-        <div>{movies.length}</div>
+        <div className="movies-grid">
+
+            {
+                movies.map(movie => (
+                    <div key={movie.id} className="movie-card">
+                        <img src={`images/${movie.image}`} alt={movie.title} />
+                        <div className="movie-card-info">
+                            <h3 className="movie-card-tile">{movie.title}</h3>
+                            <p className="movie-card-genre">{movie.genre}</p>
+                            <p className="movie-card-rating">{movie.rating}</p>
+                        </div>
+                    </div>
+                ))
+            }
+
+        </div>
     );
 }
