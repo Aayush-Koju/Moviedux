47aa82738d7ce7add1a315d8febb36c6521390f6
Aayush-Koju <iush.cozu12@gmail.com>
2024-07-29 21:29:03 +0545

    added filter bar content with drop down options

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -7,6 +7,9 @@ export default function MoviesGrid() {
 
   const [searchTerm, setSearchTerm] = useState("");
 
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
+
   useEffect(() => {
     fetch("movies.json")
       .then((response) => response.json())
@@ -15,6 +18,14 @@ export default function MoviesGrid() {
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
   };
 
   const filteredMovies = movies.filter(
@@ -30,6 +41,38 @@ export default function MoviesGrid() {
         value={searchTerm}
         onChange={handleSearchChange}
       />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
+        </div>
+
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
+
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
           <MovieCard movie={movie} key={movie.id}></MovieCard>
